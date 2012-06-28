f27c2539cea09f9874e62a63504a1a5ca39b4d62
git4rockyt <tiwarirakesh@gmail.com>
2012-06-28 18:44:55 -0500

    fixed small bug

--- runme.sh
+++ runme.sh
@@ -3,8 +3,8 @@
 
 ./ss-list.sh
 for i in `cat sorted-master.txt`; do echo "$RANDOM $i"; done | sort | sed -r 's/^[0-9]+ //' > unsorted-master.txt
-cp unsorted-master.txt run/master.inp
 mkdir run
+cp unsorted-master.txt run/master.inp
 cp -f prg.sh run/
 cd run
 ./prg.sh
